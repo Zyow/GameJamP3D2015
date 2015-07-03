b3c18fc6bc26611e54cbdc8c31cd295b0378feef
Nascaa <adrien-fograph@live.fr>
2015-07-03 22:37:31 +0200

    translocator nettoyage
    suppression de ce qui sert a rien + ajout d'une petite note

--- Assets/Scripts/Translocator.cs
+++ Assets/Scripts/Translocator.cs
@@ -9,8 +9,8 @@ public class Translocator : MonoBehaviour
 	{
 		if (col.tag == "Player1" || col.tag == "Player2" || col.tag == "Player3" || col.tag == "Player4" )
 		{
-			col.transform.position = new Vector3(otherDoor.transform.position.x, otherDoor.transform.position.y-1.2f, otherDoor.transform.position.z) ;
-//			col.GetComponent<Rigidbody>().velocity = 
+			// Penser a orienter l'axe Y dans le sens inverse de là ou le player doit spawner
+			col.transform.position = new Vector3(otherDoor.transform.position.x, otherDoor.transform.position.y-1.2f, otherDoor.transform.position.z) ; 
 		}
 	}
 }
