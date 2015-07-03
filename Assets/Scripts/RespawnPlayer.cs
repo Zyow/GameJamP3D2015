1e5e652a4e4a2c3f0f16a1fd3e85627a909b66af
Kromozome <maxime.meriaux23@gmail.com>
2015-07-03 23:24:23 +0200

    Avancement RespawnPlayer

---
+++ Assets/Scripts/RespawnPlayer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RespawnPlayer : MonoBehaviour 
+{
+	private PlayerRespawn player;
+	public float delayAddSpawnPoint;
+	public List<GameObject> respawnPoints;
+	private List<GameObject> removedPoints;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindObjectOfType<PlayerRespawn>();
+		foreach(GameObject points in GameObject.FindGameObjectsWithTag("RespawnPlayer"))
+		{
+			respawnPoints.Add(points);
+		}
+	}
+	
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			LaunchRespawn();
+		}
+	}
+	public void LaunchRespawn()
+	{
+		int spawnIndex = Random.Range(0,transform.childCount-1);
+		player.Respawn(respawnPoints[spawnIndex].transform.position);
+		//removedPoints.Add(respawnPoints[spawnIndex]);
+		GameObject removedpoint = respawnPoints[spawnIndex];
+		respawnPoints.RemoveAt(spawnIndex);
+		StartCoroutine(AddSpawnPoint(removedpoint));
+	}
+	IEnumerator AddSpawnPoint(GameObject removed)
+	{
+		print (removed.name);
+		yield return new WaitForSeconds(delayAddSpawnPoint);
+		respawnPoints.Add(removed);
+	}
+
+}
