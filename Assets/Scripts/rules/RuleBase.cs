17cfda56d2920aaeaccca0d5c9f02955b54ed6bf
wysss <wysss17@gmail.com>
2015-07-03 22:05:04 +0200

    rules
    début scripts rules

---
+++ Assets/Scripts/rules/RuleBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class RuleBase : MonoBehaviour 
+{
+	private int winner;
+	public Action<int> winnerUI;
+	public bool done;
+
+	protected void Finished ( int player)
+	{
+		winner = player;
+		if( winnerUI != null )
+			winnerUI( winner );
+	}
+}
---
+++ Assets/Scripts/rules/RuleUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RuleUI : MonoBehaviour 
+{
+	private Text text;
+
+	void Start ()
+	{
+		GetComponent<RuleBase>().winnerUI += ChangeColor;
+		text = GetComponent<Text>();
+	}
+
+	private void ChangeColor(int player )
+	{
+		switch (player)
+		{
+		case 1:
+			text.color = Color.blue;
+			break;
+
+		case 2 :
+			text.color = Color.green;
+			break;
+
+		case 3 :
+			text.color = Color.yellow;
+			break;
+
+		case 4 :
+			text.color = Color.red;
+			break;
+		}
+	}
+}
