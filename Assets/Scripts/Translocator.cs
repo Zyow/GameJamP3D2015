d348e9735f8aae7d5ba2b772817c0e3b86a5ec91
Nascaa <adrien-fograph@live.fr>
2015-07-03 22:05:40 +0200

    Add Tags
    ajout des 4 players dans les tag

---
+++ Assets/Scripts/Translocator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Translocator : MonoBehaviour 
+{
+	public GameObject[] doors;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player1" || col.tag == "Player2" || col.tag == "Player3" || col.tag == "Player4" )
+		{
+
+		}
+	}
+}
