a0867a96ce025d9462e22cb725ed44e68ce7793c
wysss <wysss17@gmail.com>
2015-07-04 03:28:17 +0200

    input + playermove + stay x time
    modif de tout les bouttons inputs
    player move refactoré
    stayXTime fini

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,14 +9,10 @@ public class PlayerMove : MonoBehaviour
 	public float speed;
 	private bool doJump;
 	public float jumpForce;
+	private int playerNbr;
 
 	private Rigidbody myRigidbody;
 	private Vector3 v3;
-
-	private Vector3 v3P1;
-	private Vector3 v3P2;
-	private Vector3 v3P3;
-	private Vector3 v3P4;
 
 	private MyCharacterController myCharacterController;
 
@@ -24,45 +20,35 @@ public class PlayerMove : MonoBehaviour
 	{
 		myRigidbody = GetComponent<Rigidbody>();
 		myCharacterController = GetComponent<MyCharacterController>();
-	}
-
-	void FixedUpdate ()
-	{
-		v3 = Vector3.forward * speed; //* 50 * Time.fixedDeltaTime;
-
-
 		switch (tag)
 		{
-			case "Player1" :
-				v3P1 = v3 * Input.GetAxis("Horizontal Player 1");
-				v3P1.y = myRigidbody.velocity.y;
-				myRigidbody.velocity = v3P1;
-				if (Input.GetButtonDown("Jump Player 1"))
-					Jump ();
-				//if (Input.GetButtonDown("Action Player 1"))
-				break;
-			case "Player2" :
-				v3P2 = v3 * Input.GetAxis("Horizontal Player 2");
-				v3P2.y = myRigidbody.velocity.y;
-				myRigidbody.velocity = v3P2;
-				if (Input.GetButtonDown("Jump Player 2"))
-					Jump ();
-				break;
-			case "Player3" :
-				v3P3 = v3 * Input.GetAxis("Horizontal Player 3");
-				v3P3.y = myRigidbody.velocity.y;
-				myRigidbody.velocity = v3P3;
-				if (Input.GetButtonDown("Jump Player 3"))
-					Jump ();
-				break;
-			case "Player4" :
-				v3P4 = v3 * Input.GetAxis("Horizontal Player 4");
-				v3P1.y = myRigidbody.velocity.y;
-				myRigidbody.velocity = v3P4;
-				if (Input.GetButtonDown("Jump Player 4"))
-					Jump ();
-				break;
+		case "Player1" :
+			playerNbr = 1;
+			break;
+		case "Player2" :
+			playerNbr = 2;
+			break;
+		case "Player3" :
+			playerNbr = 3;
+			break;
+		case "Player4" :
+			playerNbr = 4;
+			break;
 		}
+	}
+//	void Update()
+//	{
+//		if (Input.GetButtonDown("Jump Player "+playerNbr.ToString()))
+//			Jump ();
+//	}
+	void FixedUpdate ()
+	{
+		v3 = Vector3.forward * speed * Input.GetAxis("Horizontal Player " + playerNbr.ToString()); //* 50 * Time.fixedDeltaTime;
+//		v3P1 = v3 * Input.GetAxis("Horizontal Player 1");
+		v3.y = myRigidbody.velocity.y;
+		myRigidbody.velocity = v3;
+		if (Input.GetButtonDown("Jump Player "+playerNbr.ToString()))
+			Jump ();
 	}
 
 	private void Action ()
--- Assets/Scripts/rules/rules/PlayerStayXtime.cs
+++ Assets/Scripts/rules/rules/PlayerStayXtime.cs
@@ -5,22 +5,55 @@ public class PlayerStayXtime : RuleGetPlayerNbr
 {
 	private int time;
 	public int timeMax;
+	private Rigidbody myRigibody;
+	private bool isUsed;
 
 	 void Start()
 	{
 		if ( FindObjectOfType<RuleStayXTime>())
-		InvokeRepeating("TimerUp",1f,1f);
+		{
+			InvokeRepeating("TimerUp",1f,1f);
+			isUsed = true;
+		}
+		myRigibody = GetComponent<Rigidbody>();
 	}
-
-	public void TimerUp()
+	void Update()
 	{
-		time ++;
-		if (time >= timeMax)
-			FindObjectOfType<RuleStayXTime>().Done(playerNbr);
+		print(myRigibody.velocity.z);
+		if (isUsed)
+		{
+			if (myRigibody.velocity.z >= 0.1 || myRigibody.velocity.z <= -0.1)
+				TimerReset();
+			if (myRigibody.velocity.y > 0.1)
+				TimerReset();
+		}
+	}
+	private void TimerUp()
+	{
+		if (isUsed)
+		{
+			time += 1;
+			if (time >= timeMax)
+			{
+				FindObjectOfType<RuleStayXTime>().Done(playerNbr);
+				PlayerStayXtime[] players = FindObjectsOfType<PlayerStayXtime>();
+				foreach ( PlayerStayXtime player in players)
+				{
+					player.GetComponent<PlayerStayXtime>().Desactive();
+				}
+			}
+		}
+		else 
+			CancelInvoke();
 	}
 
-	public void TimerReset()
+	private void TimerReset()
 	{
 		time = 0;
+	}
+
+	public void Desactive()
+	{
+		isUsed = false;
 	}
 }
