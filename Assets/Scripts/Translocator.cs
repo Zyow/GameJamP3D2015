2c7440eda65c81af8b93eec56ca47f1e21950a5a
Zyow <szczypamaxime@gmail.com>
2015-07-04 17:12:39 +0200

    Changement Script TP + Prefab P1

--- Assets/Scripts/Translocator.cs
+++ Assets/Scripts/Translocator.cs
@@ -3,15 +3,56 @@ using System.Collections;
 
 public class Translocator : MonoBehaviour 
 {
+	public enum EnumTp
+	{
+		top,
+		left,
+		right,
+		down,
+		none,
+	};
+
 	public GameObject otherDoor;
+	public EnumTp tplocation;
 
 	void OnTriggerEnter(Collider col)
 	{
 		if (col.tag == "Player1" || col.tag == "Player2" || col.tag == "Player3" || col.tag == "Player4" )
 		{
-			// Penser a orienter l'axe Y dans le sens inverse de là ou le player doit spawner
-			col.transform.position = new Vector3(otherDoor.transform.position.x, otherDoor.transform.position.y-1.2f, otherDoor.transform.position.z) ; 
-			col.GetComponent<PlayerPassByPortals>().PassedPortal();
+
+			switch(tplocation)
+			{
+				case EnumTp.down:
+					col.transform.position = new Vector3(otherDoor.transform.position.x, otherDoor.transform.position.y-1.7f, otherDoor.transform.position.z) ; 
+					if (col.GetComponent<PlayerPassByPortals>() != null)
+						col.GetComponent<PlayerPassByPortals>().PassedPortal();
+				break;
+				case EnumTp.top:
+					col.transform.position = new Vector3(otherDoor.transform.position.x, otherDoor.transform.position.y+1.7f, otherDoor.transform.position.z) ; 
+					if (col.GetComponent<PlayerPassByPortals>() != null)
+						col.GetComponent<PlayerPassByPortals>().PassedPortal();
+				break;
+				case EnumTp.left:
+					col.transform.position = new Vector3(otherDoor.transform.position.x-1.2f, otherDoor.transform.position.y, otherDoor.transform.position.z) ; 
+					if (col.GetComponent<PlayerPassByPortals>() != null)
+						col.GetComponent<PlayerPassByPortals>().PassedPortal();
+				break;
+				case EnumTp.right:
+					col.transform.position = new Vector3(otherDoor.transform.position.x+1.2f, otherDoor.transform.position.y, otherDoor.transform.position.z) ; 
+					if (col.GetComponent<PlayerPassByPortals>() != null)
+						col.GetComponent<PlayerPassByPortals>().PassedPortal();
+				break;
+				case EnumTp.none:
+				break;
+			}
+
+//			// Penser a orienter l'axe Y dans le sens inverse de là ou le player doit spawner
+//			if (tplocation == EnumTp.down)
+//			{
+//			}
 		}
 	}
+
+
+	
 }
