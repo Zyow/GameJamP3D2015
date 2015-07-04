c1fcdca1525a565b1deffdd1961f48340f2628d0
eskivor <pierre.stempin@gmail.com>
2015-07-04 12:54:54 +0200

    rule first hit

---
+++ Assets/Scripts/rules/rules/first hit/PlayerFirstHit.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFirstHit : MonoBehaviour 
+{
+	private bool isUsed;
+	private int timeHit;
+	
+	void Start ()
+	{
+		if (FindObjectOfType<RuleFirstHit>())
+			isUsed = true;
+		GetComponent<PlayerHit>().hit += Hit;
+	}
+
+	private void Hit ()
+	{
+		timeHit ++;
+		if (isUsed == true && timeHit >= 1)
+		{
+			FindObjectOfType<RuleFirstHit>().Done(playerNbr);
+			
+			PlayerFirstHit[] players = FindObjectsOfType<PlayerFirstHit>();
+			foreach ( PlayerFirstHit player in players)
+			{
+				player.GetComponent<PlayerFirstHit>().Desactive();
+			}
+		}
+
+	}
+
+	public void Desactive ()
+	{
+		isUsed = false;
+		GetComponent<PlayerMove>().hit -= Hit;
+	}
+}
