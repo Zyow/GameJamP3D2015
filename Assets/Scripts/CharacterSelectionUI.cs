43036c1c4e865e528267c503a7efaddb4f45062b
eskivor <pierre.stempin@gmail.com>
2015-07-05 01:23:56 +0200

    character selection suite

--- Assets/Scripts/CharacterSelectionUI.cs
+++ Assets/Scripts/CharacterSelectionUI.cs
@@ -11,6 +11,16 @@ public class CharacterSelectionUI : MonoBehaviour
 	private bool canLaunchGame;
 	private LoadingPlayer loadingPlayer;
 
+	public GameObject textPlayer1Join;
+	public GameObject textPlayer2Join;
+	public GameObject textPlayer3Join;
+	public GameObject textPlayer4Join;
+
+	public GameObject textPlayer1Joined;
+	public GameObject textPlayer2Joined;
+	public GameObject textPlayer3Joined;
+	public GameObject textPlayer4Joined;
+
 	void Start ()
 	{
 		Instantiate (loadingPlayerPrefab);
@@ -33,6 +43,18 @@ public class CharacterSelectionUI : MonoBehaviour
 		if (Input.GetButtonDown ("Jump Player 4"))
 			ActivePlayer4 ();
 
+		if (Input.GetButtonDown ("Hit Player 1"))
+			ActivePlayer1 ();
+		
+		if (Input.GetButtonDown ("Hit Player 2"))
+			ActivePlayer2 ();
+		
+		if (Input.GetButtonDown ("Hit Player 3"))
+			ActivePlayer3 ();
+		
+		if (Input.GetButtonDown ("Hit Player 4"))
+			ActivePlayer4 ();
+
 		if (Input.GetButtonDown ("Pause") && canLaunchGame)
 		{
 			Application.LoadLevel("SceneRespawnPlayer");
@@ -48,12 +70,20 @@ public class CharacterSelectionUI : MonoBehaviour
 			cantLaunchGameUI.SetActive (false);
 			canLaunchGameUI.SetActive (true);
 		}
+		else
+		{
+			canLaunchGame = false;
+			cantLaunchGameUI.SetActive (true);
+			canLaunchGameUI.SetActive (false);
+		}
 	}
 
 	public void ActivePlayer1 ()
 	{
 		if (!loadingPlayer.playerOneActive)
 		{
+			textPlayer1Join.SetActive (false);
+			textPlayer1Joined.SetActive (true);
 			numberActivePlayers ++;
 			loadingPlayer.playerOneActive = true;
 			CheckNumberPlayers ();
@@ -64,6 +94,8 @@ public class CharacterSelectionUI : MonoBehaviour
 	{
 		if (!loadingPlayer.playerTwoActive)
 		{
+			textPlayer2Join.SetActive (false);
+			textPlayer2Joined.SetActive (true);
 			numberActivePlayers ++;
 			loadingPlayer.playerTwoActive = true;
 			CheckNumberPlayers ();
@@ -74,6 +106,8 @@ public class CharacterSelectionUI : MonoBehaviour
 	{
 		if (!loadingPlayer.playerTreeActive)
 		{
+			textPlayer3Join.SetActive (false);
+			textPlayer3Joined.SetActive (true);
 			numberActivePlayers ++;
 			loadingPlayer.playerTreeActive = true;
 			CheckNumberPlayers ();
@@ -84,8 +118,58 @@ public class CharacterSelectionUI : MonoBehaviour
 	{
 		if (!loadingPlayer.playerFourActive)
 		{
+			textPlayer4Join.SetActive (false);
+			textPlayer4Joined.SetActive (true);
 			numberActivePlayers ++;
 			loadingPlayer.playerFourActive = true;
+			CheckNumberPlayers ();
+		}
+	}
+
+	public void DesactivePlayer1 ()
+	{
+		if (loadingPlayer.playerOneActive)
+		{
+			textPlayer1Join.SetActive (true);
+			textPlayer1Joined.SetActive (false);
+			numberActivePlayers --;
+			loadingPlayer.playerOneActive = false;
+			CheckNumberPlayers ();
+		}
+	}
+	
+	public void DesactivePlayer2 ()
+	{
+		if (loadingPlayer.playerTwoActive)
+		{
+			textPlayer2Join.SetActive (true);
+			textPlayer2Joined.SetActive (false);
+			numberActivePlayers --;
+			loadingPlayer.playerTwoActive = false;
+			CheckNumberPlayers ();
+		}
+	}
+	
+	public void DesactivePlayer3 ()
+	{
+		if (loadingPlayer.playerTreeActive)
+		{
+			textPlayer3Join.SetActive (true);
+			textPlayer3Joined.SetActive (false);
+			numberActivePlayers --;
+			loadingPlayer.playerTreeActive = false;
+			CheckNumberPlayers ();
+		}
+	}
+	
+	public void DesactivePlayer4 ()
+	{
+		if (loadingPlayer.playerFourActive)
+		{
+			textPlayer4Join.SetActive (true);
+			textPlayer4Joined.SetActive (false);
+			numberActivePlayers --;
+			loadingPlayer.playerFourActive = false;
 			CheckNumberPlayers ();
 		}
 	}
