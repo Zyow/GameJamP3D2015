c9de8d02e322bacaa3739cfc30790a130cfc6ea4
wysss <wysss17@gmail.com>
2015-07-04 15:32:12 +0200

    rule Cage

--- Assets/Scripts/Cage.cs
+++ Assets/Scripts/Cage.cs
@@ -5,12 +5,13 @@ public class Cage : MonoBehaviour
 {
 	public int life;
 
-	public void TakeHit()
+	public void TakeHit(Transform player)
 	{
 		life --;
 		if (life == 0)
 		{
 			Destroy(gameObject);
+			GetComponent<CageBreak>().Breaking(player.GetComponentInParent<Transform>().tag);
 		}
 	}
 }
---
+++ Assets/Scripts/rules/rules/BreakTheCage/CageBreak.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CageBreak : MonoBehaviour 
+{
+	private bool isUsed ;
+	
+	void Start ()
+	{
+		if ( FindObjectOfType<RuleBreakCage>())
+			isUsed = true;
+		
+	}
+	
+	public void Breaking(string player)
+	{
+		if (isUsed)
+		{
+			int playerNbr;
+			switch (player)
+			{
+			case "Player1" :
+				playerNbr = 1;
+				break;
+			case "Player2" :
+				playerNbr = 2;
+				break;
+			case "Player3" :
+				playerNbr = 3;
+				break;
+			case "Player4" :
+				playerNbr = 4;
+				break;
+			}
+
+			FindObjectOfType<RuleBreakCage>().Done(playerNbr);			
+		}
+	}
+}
