f60eac5e34ac5a9519453b9fc4e7b1379c157310
Nascaa <adrien-fograph@live.fr>
2015-07-04 11:13:28 +0200

    bump
    script du collider qui fait bump lennemi

---
+++ Assets/Scripts/HitCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HitCollider : MonoBehaviour 
+{
+	public Action<string> enemyHited;
+
+	private float ejectionForce = 3f;
+
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player1" || col.tag == "Player2" || col.tag == "Player3" || col.tag == "Player4" )
+		{
+			print ("col");
+			col.GetComponent<Rigidbody>().AddForce(Vector3.up * ejectionForce, ForceMode.Impulse);
+			col.GetComponent<Rigidbody>().AddForce(Vector3.forward * ejectionForce*2f, ForceMode.Impulse);
+			if(enemyHited != null)
+				enemyHited(col.tag);
+		}
+
+	}
+}
