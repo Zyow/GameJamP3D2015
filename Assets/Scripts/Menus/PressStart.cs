76f651a10eb33fb782155ee747e9395f4a02d02c
eskivor <pierre.stempin@gmail.com>
2015-07-05 17:00:58 +0200

    Changmenent Loading Menu + Sprite Character Concept

---
+++ Assets/Scripts/Menus/PressStart.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressStart : MonoBehaviour 
+{
+
+	void Update()
+	{
+		if(Input.GetButtonDown("Pause"))
+		{
+			Application.LoadLevel ("SceneCharacterSelection");
+		}
+	}
+	
+}
