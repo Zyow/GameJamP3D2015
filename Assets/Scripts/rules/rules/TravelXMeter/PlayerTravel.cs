4ff70d74e9a480ca36f2a34b616cbe1dbdc32b97
wysss <wysss17@gmail.com>
2015-07-05 14:11:28 +0200

    travel X meter

---
+++ Assets/Scripts/rules/rules/TravelXMeter/PlayerTravel.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTravel : PlayerBase 
+{
+	private bool isUsed;
+	private float distance;
+	public float distanceMax;
+	
+	void Start ()
+	{
+		if (FindObjectOfType<RuleTravel>())
+			isUsed = true;
+	}
+	
+	public void FixedUpdate ()
+	{
+		if (isUsed == true)
+		{
+			distance += Mathf.Abs((GetComponent<Rigidbody>().velocity.x)/100f);
+			if (distance >= distanceMax)
+			{
+				FindObjectOfType<RuleTravel>().Done(playerNbr);
+				
+				PlayerTravel[] players = FindObjectsOfType<PlayerTravel>();
+				foreach ( PlayerTravel player in players)
+				{
+					player.GetComponent<PlayerTravel>().Desactive();
+				}
+			}
+		}
+	}
+	
+	public void Desactive ()
+	{
+		isUsed = false;
+	}
+}
---
+++ Assets/Scripts/rules/rules/TravelXMeter/RuleTravel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuleTravel : RuleBase 
+{
+	public void Done(int player)
+	{
+		Finished(player);
+	}
+}
