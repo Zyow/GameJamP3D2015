e9fae922e38479237c779dbd5476530330b30432
Nascaa <adrien-fograph@live.fr>
2015-07-05 13:50:54 +0200

    la dance ok cool wesh

---
+++ Assets/Scripts/Dance.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dance : MonoBehaviour 
+{
+	
+	public void DancePosition () 
+	{
+		transform.parent.transform.rotation = Quaternion.Euler(0f, 180, 0f);
+	}
+}
