b57d47b13a088b4f748a14ef43403e6e657cfd98
eskivor <pierre.stempin@gmail.com>
2015-07-05 15:06:59 +0200

    audio manager ok !!!

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -6,21 +6,48 @@ public class AudioManager : MonoBehaviour
 	public AudioClip gameMusic;
 	public AudioClip menuMusic;
 	private AudioSource audioSource;
+	public static AudioManager instance;
 
 	void Start ()
 	{
-		DontDestroyOnLoad (gameObject);
-		audioSource = GetComponent<AudioSource>();
-		audioSource.clip = menuMusic;
+		if (instance == null)
+		{
+			DontDestroyOnLoad (gameObject);
+			instance = this;
+			audioSource = GetComponent<AudioSource>();
+			MenuLaunched ();
+		}
+		else if ( instance != this)
+		{
+			Destroy (gameObject);
+		}
+
+		/*if(GameObject.FindObjectOfType<AudioSource>() != null)
+		{
+			Destroy (gameObject);
+		}*/
+		//DontDestroyOnLoad (gameObject);
+	}
+
+	void Update ()
+	{
+		//cheat
+		if (Input.GetKeyDown(KeyCode.Y))
+			GameLaunched ();
+
+		if (Input.GetKeyDown(KeyCode.U))
+			MenuLaunched ();
 	}
 
 	public void GameLaunched ()
 	{
 		audioSource.clip = gameMusic;
+		audioSource.Play ();
 	}
 
-	public void GameEnd ()
+	public void MenuLaunched ()
 	{
 		audioSource.clip = menuMusic;
+		audioSource.Play ();
 	}
 }
