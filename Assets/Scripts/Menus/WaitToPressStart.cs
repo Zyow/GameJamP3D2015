1fdc372050b22d022beb403eead0b3523e8243ad
Nascaa <adrien-fograph@live.fr>
2015-07-07 15:21:21 +0200

    New menu titre

---
+++ Assets/Scripts/Menus/WaitToPressStart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitToPressStart : MonoBehaviour 
+{
+	public GameObject startObject;
+
+	void Awake () 
+	{
+		startObject.SetActive(false);
+		StartCoroutine(WaitStart());
+	}
+	
+	// Update is called once per frame
+	private IEnumerator WaitStart () 
+	{	
+		yield return new WaitForSeconds(2f);
+		startObject.SetActive(true);
+	}
+}
